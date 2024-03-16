175b970a2f5991725469c64523f6d148ef5cc1d5
Jose Estrella <dev.estrella.jc@gmail.com>
2024-03-16 19:34:47 -0400

    first files

---
+++ 001 Adding PHP to my website/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP in html</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+
+    <?php
+    // This is a comment in PHP
+        echo "<h2>This is PHP " . time() .  "</h2>"; 
+    
+    /* This is a multi-line comment
+    in PHP */
+    
+    # This is also a single line comment
+
+    ?>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php 
+echo"Hello World! ";
+echo "Muevete entre carpetas para ver el contenido de cada una de ellas";
